2543f7d9e6771b9bf4261762cf2bc6c4798bdb74
Traye <johnson.traye@yahoo.com>
2021-04-27 16:46:50 -0500

    added login and register

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Metroid } from './components/Metroid';
-import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
   <React.StrictMode>
